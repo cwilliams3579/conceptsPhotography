e220a12fe1d8cf7f617e4490da89c82b8ad53d45
cwilliams3579 <clevelandwilliams3579@gmail.com>
2017-06-29 07:25:08 -0400

    changed body color

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { :host => 'concievedconceptshphotographs.com', :protocol => 'https' }
+  # Rails.application.config.assets.precompile += %w( *.js ^[^_]*.css *.css.erb )
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -29,7 +29,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -89,4 +89,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { :host => 'concievedconceptshphotographs.com', :protocol => 'https' }
 end
