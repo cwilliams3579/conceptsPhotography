7023d445a727fdfdaf0406e96ce50d660ce6ce1a
cwilliams3579 <clevelandwilliams3579@gmail.com>
2017-07-08 20:11:34 -0400

    add figaro

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,20 +1,9 @@
-require 'carrierwave/orm/activerecord'
-if Rails.env.production?
-
-  CarrierWave.configure do |config|
-
-    config.fog_credentials = {
-
-      :provider => 'AWS',
-
-      :aws_access_key_id => ENV['S3_ACCESS_KEY'],
-
-      :aws_secret_access_key => ENV['S3_SECRET_KEY']
-
-    }
-
-    config.fog_directory = ENV['S3_BUCKET']
-
-  end
-
+CarrierWave.configure do |config|
+  config.fog_provider = 'fog/aws'                        # required
+  config.fog_credentials = {
+    provider:              'AWS',                        # required
+    aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],                        # required
+    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],                        # required
+  }
+  config.fog_directory  = ENV["fog_directory"]                          # required
 end
