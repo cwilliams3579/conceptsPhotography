f25a359d8f98910055e8f278f420be4f25cac758
cwilliams3579 <clevelandwilliams3579@gmail.com>
2017-07-29 18:48:51 -0400

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
  # mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  root to: 'visitors#index'
+  root to: 'posts#index'
   devise_for :users, :controllers => { registrations: 'registrations'}
   resources :galleries
   resources :posts do
