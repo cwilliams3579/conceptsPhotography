4e0e381a71c0298aef337cc69dfcdb85213c67de
cwilliams3579 <clevelandwilliams3579@gmail.com>
2017-08-04 06:55:27 -0400

    add cancancanabiity

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  load_and_authorize_resource
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.order("created_at DESC")
