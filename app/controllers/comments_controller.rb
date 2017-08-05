1025dbf028bacce53ff5c947ad4651df5776ba8a
cwilliams3579 <clevelandwilliams3579@gmail.com>
2017-08-04 21:37:44 -0400

    fix comment form and friendly id

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,16 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
 
-  def create
-    @post = Post.find(params[:post_id])
-    @comment = @post.comments.new(comment_params)
-    @comment.user = current_user
-    @comment.save!
 
-    respond_to do |format|
-      format.html { redirect_to @post }
-      format.js # render comments/create.js.erb
+  def create
+    @post = Post.friendly.find(params[:post_id])
+    @comment = @post.comments.build(comment_params)
+    @comment.user = current_user
+    if @comment.save 
+      respond_to do |format|
+        format.html { redirect_to @post }
+        format.js # render comments/create.js.erb
+      end
     end
   end
 
@@ -25,7 +26,7 @@ class CommentsController < ApplicationController
 
   private
 
-  def comment_params
-    params.require(:comment).permit(:content, :user_id, :post_id)
-  end
+    def comment_params
+      params.require(:comment).permit(:content, :user_id, :post_id)
+    end
 end
